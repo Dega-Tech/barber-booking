ba72696617a0c25d49a3466e6f7ab4726d9b8148
Malchiel <Malchiel.dawit1@gmail.com>
2026-07-01 16:43:58 +0300

    feat:integrate tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div>barber-booking</div>
-  )
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline">
+        Hello Tailwind!
+      </h1>
+    </div>
+  );
 }
-export default App
+
+export default App;
 
